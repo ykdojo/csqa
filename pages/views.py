5d924b6c942c1fdd304b634e4ee8e1225d390bf1
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-12 22:17:09 -0700

    Showing the username on the UI

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 def homePageView(request):
-    return render(request, 'home.html')
+    current_user = request.user
+    if not current_user.is_authenticated:
+        current_user = None
+    context = {'current_user': current_user}
+    return render(request, 'home.html', context)
 
 def aboutPageView(request):
     return render(request, 'about.html')
