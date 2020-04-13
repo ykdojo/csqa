0b64d8a6fb6ec1165fb3b0fb59227f754f964a6c
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-12 17:51:35 -0700

    Added a top level templates dir

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'home.html')
