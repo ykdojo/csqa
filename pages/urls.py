04cb52e7b4728901a0651eec234e77d5bd461527
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-12 17:42:33 -0700

    The hello world works

---
+++ pages/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import homePageView
+
+urlpatterns = [
+    path('', homePageView)
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def homePageView(request):
+    return HttpResponse('Hello, World!')
