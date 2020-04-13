f4cb15bca76b821b690c02f79ceec057700e4100
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-12 19:05:22 -0700

    Update settings.py

--- csqa_project/settings.py
+++ csqa_project/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'csqa_project.urls'
