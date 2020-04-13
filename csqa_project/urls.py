fa9c6f2fc9a7558772a53428be1a60900c7c00c9
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-13 14:17:57 -0700

    Added questions in db, changed the URL scheme

--- csqa_project/urls.py
+++ csqa_project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pages.views import homePageView, aboutPageView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('', homePageView),
+    path('about/', aboutPageView),
     path('accounts/', include('allauth.urls')),
 ]
--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import homePageView, aboutPageView
-
-urlpatterns = [
-    path('', homePageView),
-    path('about', aboutPageView),
-]
