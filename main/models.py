583f8df5bcf042f6c03333a79fe63737b2fe5824
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-13 14:54:00 -0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
