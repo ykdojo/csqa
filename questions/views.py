768bee21e60de054bb4b39907c90017f303be4e9
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-13 16:29:28 -0700

    Created a new question page

--- questions/views.py
+++ questions/views.py
@@ -4,3 +4,6 @@ from main.models import Question
 def questionView(request, id):
     question = Question.objects.get(pk=id)
     return render(request, 'question.html', {'question': question})
+
+def newView(request):
+    return render(request, 'new.html')
