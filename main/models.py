7b5f90a434eaadc55e844bb99486cf7d6c5ac76b
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-14 23:36:47 -0700

    Updated the Answer model

--- main/models.py
+++ main/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import User
 from django import forms
 import datetime
 
+# Taking a time difference as inputs, it returns a string like:
+# '223 days ago'
+# '3 hours ago'
+# '23 minutes ago'
+# '20 seconds ago'
+def x_ago_helper(diff):
+    if diff.days > 0:
+        return f'{diff.days} days ago'
+    if diff.seconds < 60:
+        return f'{diff.seconds} seconds ago'
+    if diff.seconds < 3600:
+        return f'{diff.seconds // 60} minutes ago'
+    return f'{diff.seconds // 3600} hours ago'
+
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -10,23 +24,10 @@ class Question(models.Model):
     created     = models.DateTimeField(editable=False)
     modified    = models.DateTimeField()
     
-    # Return a string like:
-    # '223 days ago'
-    # '3 hours ago'
-    # '23 minutes ago'
-    # '20 seconds ago'
     @property
     def x_ago(self):
         diff = datetime.datetime.now(datetime.timezone.utc) - self.created
-        if diff.days > 0:
-            return f'{diff.days} days ago'
-        if diff.seconds < 60:
-            return f'{diff.seconds} seconds ago'
-        if diff.seconds < 3600:
-            return f'{diff.seconds // 60} minutes ago'
-        return f'{diff.seconds // 3600} hours ago'
-        
-
+        return x_ago_helper(diff)
         
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
@@ -45,6 +46,21 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
+    created = models.DateTimeField(editable=False)
+    modified = models.DateTimeField()
+
+    @property
+    def x_ago(self):
+        diff = datetime.datetime.now(datetime.timezone.utc) - self.created
+        return x_ago_helper(diff)
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = datetime.datetime.now()
+        self.modified = datetime.datetime.now()
+        return super(Answer, self).save(*args, **kwargs)
+    
     def __str__(self):
         return self.text
 
