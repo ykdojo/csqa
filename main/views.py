8c98e22a650bf2dfbedb555e07ea9b560c4559f1
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-16 22:12:10 -0700

    showing hostname (for testing)

--- main/views.py
+++ main/views.py
@@ -4,11 +4,13 @@ from main.models import Question
 def homeFeedView(request):
     current_user = request.user
     
+    hostname = request.get_host()
     questions = Question.objects.all().order_by('-created')[:30]
     questions_exist = len(questions) > 0
     context = {
         'current_user': current_user,
         'questions': questions,
-        'questions_exist': questions_exist
+        'questions_exist': questions_exist,
+        'hostname': hostname
     }
     return render(request, 'home.html', context)
