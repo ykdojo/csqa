718ff45275902ed46e7201548860370ec40157c0
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-16 18:19:49 -0700

    Added Postgresql + style fixes

--- csqa_project/settings.py
+++ csqa_project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'csqa_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ.get('POSTGRES_HOST', default='localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', default='5432')
     }
 }
 
--- main/views.py
+++ main/views.py
@@ -6,8 +6,10 @@ def homeFeedView(request):
     
     # TODO: Only retrieve recent questions.
     questions = Question.objects.all().order_by('-created')[:30]
+    questions_exist = len(questions) > 0
     context = {
         'current_user': current_user,
-        'questions': questions
+        'questions': questions,
+        'questions_exist': questions_exist
     }
     return render(request, 'home.html', context)
