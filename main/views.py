d825148367c636d1b64ec5f01f507082a662987f
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-17 00:05:00 -0700

    Revert "Revert "Got rid of the host name""
    This reverts commit 4ed737b1d7d24b0c0c8491574497e41ea2a39b6e.

--- main/views.py
+++ main/views.py
@@ -4,13 +4,11 @@ from main.models import Question
 def homeFeedView(request):
     current_user = request.user
     
-    hostname = request.get_host()
     questions = Question.objects.all().order_by('-created')[:30]
     questions_exist = len(questions) > 0
     context = {
         'current_user': current_user,
         'questions': questions,
-        'questions_exist': questions_exist,
-        'hostname': hostname
+        'questions_exist': questions_exist
     }
     return render(request, 'home.html', context)
