d93607e5fe21823a7aa29068a95541daae5f22c8
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-17 11:47:49 -0700

    Added paginator

--- main/views.py
+++ main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from main.models import Question
+from django.core.paginator import Paginator
+
+def listing(request):
+    return render(request, 'list.html', {'page_obj': page_obj})
 
 def homeFeedView(request):
     current_user = request.user
     
-    questions = Question.objects.all().order_by('-created')[:30]
+    questions = Question.objects.all().order_by('-created')
+    paginator = Paginator(questions, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     questions_exist = len(questions) > 0
     context = {
         'current_user': current_user,
-        'questions': questions,
+        'page_obj': page_obj,
         'questions_exist': questions_exist
     }
     return render(request, 'home.html', context)
