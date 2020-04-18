e081e58c13e6c00600424c4660aecdd6f38cde10
YK Sugi (CS Dojo) <yksu4p@gmail.com>
2020-04-17 17:45:32 -0700

    Revert "CustomUser defined"
    This reverts commit 5c1928f51d09ba51eec102c655c4f62f706d6206.

--- users/models.py
+++ users/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser, UserManager
-from django.apps import apps
-
-class CustomUserManager(UserManager):
-    pass
-
-class CustomUser(AbstractUser):
-    objects = CustomUserManager()
-    upvoted_questions = models.ManyToManyField('main.Question', related_name="upvoted_users")
-    downvoted_questions = models.ManyToManyField('main.Question', related_name="downvoted_users")
+# Create your models here.
